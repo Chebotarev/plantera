7285599cef6867ad69bfaa7de16e412da2b45d2e
Chebotarev <aleksey.chebotarev@gmail.com>
2021-03-14 14:00:26 -0700

    Generate Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+
+  root to: 'home#index'
+  get 'home/index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
