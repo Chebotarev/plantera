eeac24b53e3fd2fc751d653e925274842f246a38
Chebotarev <aleksey.chebotarev@gmail.com>
2021-03-21 22:58:26 -0700

    Address devise bug with protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
-
     # Overwriting the sign_out redirect path method
     def after_sign_out_path_for(resource_or_scope)
         new_user_session_url
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   before_action :set_disable_header
-  skip_before_action :verify_authenticity_token, only: :create
+  protect_from_forgery with: :null_session
 
   protected
 
